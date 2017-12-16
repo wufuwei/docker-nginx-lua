b7ff892323d92fd0970a5f30ef3124a11f06b3e2
wufuwei <oradb@163.com>
2017-12-16 17:14:03 +0800

    修改nginx为copy request

--- lua/copy_req.lua
+++ lua/copy_req.lua
@@ -1,30 +1,27 @@
-local res1, res2, action
+local online, test, action
 action = ngx.var.request_method
+
 if action == "POST" then
-	arry = {method = ngx.HTTP_POST, body = request_body}
+    ngx.req.read_body() -- 解析 body 参数之前一定要先读取 body
+    local arg = ngx.req.get_post_args() -- post需要参数传递
+    arry = {method = ngx.HTTP_POST, body = ngx.var.request_body, args=''}
 else
-	arry = {method = ngx.HTTP_GET}
+    arry = {method = ngx.HTTP_GET}
 end
 
 if ngx.var.svr == "on" then
-	res1, res2 = ngx.location.capture_multi {
-		{ "/iot" .. ngx.var.request_uri , arry},
-		{ "/agent-wy" .. ngx.var.request_uri , arry},
-	}	
+  online, test = ngx.location.capture_multi {
+   { "/online" .. ngx.var.request_uri, arry},
+   { "/test" .. ngx.var.request_uri, arry},
+ }
 else
-	res1, res2 = ngx.location.capture_multi {
-		{ "/iot" .. ngx.var.request_uri , arry},
-	}
+   online = ngx.location.capture_multi {
+       { "/online" .. ngx.var.request_uri, arry}
+   }
 end
 
-if res1.status == ngx.HTTP_OK then
-	local header_list = {"Content-Length", "Content-Type", "Content-Encoding", "Accept-Ranges"}
-	for _, i in ipairs(header_list) do
-		if res1.header[i] then
-			ngx.header[i] = res1.header[i]
-		end
-	end
-	ngx.say(res1.body)
+if online.status == ngx.HTTP_OK then -- 只返回online server的结果
+  ngx.say(online.body)
 else
-	ngx.status = ngx.HTTP_NOT_FOUND
+  ngx.status = ngx.HTTP_NOT_FOUND
 end
